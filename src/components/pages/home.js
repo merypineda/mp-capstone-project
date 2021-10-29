716ee974cdedaa7fc140f3afd2e91694022b6469
merypineda <meryp2@icloud.com>
2021-10-28 23:49:22 -0400

    updated the home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -9,18 +9,19 @@ export default class Home extends Component {
 
 render() {
         return (
-            <div className="home-wrapper">
+            <div className="home-wrapper-container">
               <NavBar />
-              <h1>Get inspired with quotes</h1>
-                <div className="home-wrapper-contents"> 
-                    <Link className="homepage-quote" to="/create">Create Your Own</Link> 
-                    <div className="homepage-quote-box">  
-                        <div className="homepage-quote-text">
-                            <p> “It always seems impossible until it's done.” Nelson Mandela</p>
+                <h1>Get inspired with quotes</h1>
+                    <div className="home-wrapper-contents">
+                        <div className="home-wrapper-body">
+                        <Link className="home-wrapper-quote" to="/create">Create Your Own</Link> 
+                        <div className="home-wrapper-quote-box">  
+                            <div className="home-wrapper-quote-text">
+                                <p> “It always seems impossible until it's done.” Nelson Mandela</p>
+                                </div>
                             </div>
-
-                    </div>
-                    </div>
+                        </div>
+                        </div>
                 </div>
       
         )
